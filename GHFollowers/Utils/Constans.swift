23319101787eb43d85189dd9bba9be6f365ea776
ChangVictor <v.chang.yu@gmail.com>
2024-02-29 20:51:10 +0100

    added item info view

--- GHFollowers/Utils/Constans.swift
+++ GHFollowers/Utils/Constans.swift
@@ -9,4 +9,8 @@ import Foundation
 
 enum SFSymbols {
     static let location = "mappin.and.ellipse"
+    static let repos = "repos"
+    static let gists = "text.alignleft"
+    static let followers = "herat"
+    static let following = "person.2"
 }
