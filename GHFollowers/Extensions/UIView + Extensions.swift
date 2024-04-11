4d4d7cec81c82c64f5060526e431ec297736e1fa
ChangVictor <v.chang.yu@gmail.com>
2024-04-11 12:02:38 +0200

    refactored user info VC into scrollview to fit iPhoneSE layout

--- GHFollowers/Extensions/UIView + Extensions.swift
+++ GHFollowers/Extensions/UIView + Extensions.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 extension UIView {
+    
     func addSubviews(_ views: UIView...) {
         for view in views { addSubviews(view) }
+    }
+    
+    func pinToEdges(of superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor)
+        ])
     }
 }
